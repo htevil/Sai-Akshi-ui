768a45cdd056bc6821ddde983c537d19852f6ad6
Harshit Tiwari <99743898+htevil@users.noreply.github.com>
2024-10-04 19:18:36 +0530

    Ninth commit

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,7 +2,7 @@ import logo from "../../asset/logo.png";
 
 export default function Navbar() {
     return (
-        <div className="navbar" style={{ width: "100%", height:"fit-content", position: "fixed", top: "0%", backgroundColor: "#D9EDBF",  display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div className="navbar" style={{ width: "100%", height:"fit-content", position: "fixed", top: "0%", backgroundColor: "#D9EDBF",  display: "flex", justifyContent: "center", alignItems: "center", zIndex:"20"}}>
             <div style={{ width: "100%", maxWidth: "1560px", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "8px 40px 10px"}}>
                 <button className="logo-button" onClick={() => window.location.href = "/"} style={{ backgroundColor: "transparent", border: "none", }}>
                     <img src={logo} className="logos" alt="no_logo" style={{ width: "120px", backgroundColor: "transparent" }} />
